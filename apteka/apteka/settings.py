3addc0aba8b90d7280e6b5ac716d8db7373853ce
DuckTheDukc <166407070+DuckTheDukc@users.noreply.github.com>
2025-04-09 14:20:06 +0400

    finally user login and controller🐲

--- apteka/apteka/settings.py
+++ apteka/apteka/settings.py
@@ -144,6 +144,7 @@ INTERNAL_IPS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "users.User"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
