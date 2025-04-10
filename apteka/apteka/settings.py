7ee09c2cc63164644a26bf1c4acce2b7c0da7e5c
DuckTheDukc <166407070+DuckTheDukc@users.noreply.github.com>
2025-04-10 20:14:46 +0400

    (mario) cart🤡

--- apteka/apteka/settings.py
+++ apteka/apteka/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "main",
     "products",
     "users",
+    "carts",
 ]
 
 MIDDLEWARE = [
@@ -145,6 +146,8 @@ INTERNAL_IPS = [
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "users.User"
+LOGIN_URL = "/user/login"
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
--- apteka/users/views.py
+++ apteka/users/views.py
@@ -17,7 +17,12 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f"Здраствуйте, {username}")
+                redirect_page = request.POST.get("next", None)
+                if redirect_page and redirect_page != reverse("user:logout"):
+                    return HttpResponseRedirect(request.POST.get("next"))
+
                 return HttpResponseRedirect(reverse("main:index"))
+
     else:
         form = UserLoginForm()
 
@@ -88,3 +93,7 @@ def logout(request):
     auth.logout(request)
     messages.success(request, f"Вы вышли из аккаунта")
     return redirect(reverse("main:index"))
+
+
+def users_cart(request):
+    return render(request, "users/users_cart.html")
