0a0882c337f261a0172a72aa3f0699596774d4ef
DuckTheDukc <166407070+DuckTheDukc@users.noreply.github.com>
2025-04-11 15:57:34 +0400

    аптека нового поколения ребилд 2+1

--- apteka/products/admin.py
+++ apteka/products/admin.py
@@ -9,8 +9,24 @@ from products.models import Categories, Products
 @admin.register(Categories)
 class CaterogiesAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
+    list_display = [
+        "name",
+    ]
 
 
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
+    list_display = ["name", "quantity", "price", "discount"]
+    list_editable = [
+        "discount",
+    ]
+    search_fields = [
+        "name",
+        "description",
+    ]
+    list_filter = [
+        "discount",
+        "quantity",
+        "category",
+    ]
